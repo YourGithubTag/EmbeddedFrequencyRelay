4ac8a48c087a5abf516104ef41b25ab45959e9a9
Nikhil <nikhil@pop-os.localdomain>
2021-04-21 17:24:11 +1200

    adding to main.h

---
+++ Software/Assignment01/main.h
@@ -0,0 +1,10 @@
+QueueHandle_t msgqueue;
+
+// used to delete a task
+TaskHandle_t xHandle;
+
+// Definition of Semaphore
+SemaphoreHandle_t shared_resource_sem;
+
+// globals variables
+QueueHandle_t newLoadQ ;
