c595aefedfff4640109d57122671acefb04c1e0f
Cecil Symes <57969872+CCP115@users.noreply.github.com>
2021-04-21 18:44:52 +1200

    Update main.c

--- Software/Assignment01/main.c
+++ Software/Assignment01/main.c
@@ -1,3 +1,23 @@
+// COMPSYS 723 Assignment 1
+// Cecil Symes, Nikhil Kumar
+// csym531, nkmu576
+
+/* Standard includes. */
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Scheduler includes. */
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+
+/* HAL API includes */
+#include "system.h"
+#include "sys/alt_irq.h"
+#include "io.h"
+#include "altera_avalon_pio_regs.h"
 
 // Definition of Task Stacks
 #define   TASK_STACKSIZE       2048
@@ -23,7 +43,24 @@ SemaphoreHandle_t shared_resource_sem;
 
 // globals variables
 QueueHandle_t newLoadQ ;
-int main(int argc, char* argv[], char* envp[]){
+
+// ISR for handling Frequency Relay Interrupt
+void freq_relay(){
+	unsigned int temp = IORD(FREQUENCY_ANALYSER_BASE, 0);
+	printf("%f Hz\n", 16000/(double)temp);
+	return;
+}
+
+int main(int argc, char* argv[], char* envp[])
+{
+	// Register interrupt for frequency analyser component
+	alt_irq_register(FREQUENCY_ANALYSER_IRQ, 0, freq_relay);
+
+	// Start Scheduler
+	vTaskStartScheduler();
+
+	// Program will only reach here if insufficient heap to start scheduler
+	for(;;)
     return 0;
 }
 
