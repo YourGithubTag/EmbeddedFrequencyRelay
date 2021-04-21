6a2ba57ec965d7a34890be0233f8538d6ebee1d9
Nik <youremailid@protonmail.com>
2021-04-21 21:00:50 +1200

    Wall Switch states

--- Software/Assignment01/main.c
+++ Software/Assignment01/main.c
@@ -51,33 +51,60 @@ SemaphoreHandle_t shared_resource_sem;
 QueueHandle_t newLoadQ ;
 
 
+static void load_Manage(void *pvParameters) {
+
+	int temp = 0;
+	int switches[7]; 
+	int iter = 0;
+
+	while (1) {
+			if (xQueueReceive(newLoadQ, &temp, portMAX_DELAY) == pdTRUE) {
+
+				
+				iter = 0;
+				for (int i = 1; i <= 64 ; i = i * 2) {
+					if ( (temp & i) != 0 ) {
+						switches[iter] = 1;
+					} else {
+						switches[iter] = 0;
+					}
+					iter++;
+				}
+
+				printf("%d, %d, %d, %d, %d, %d, %d \n", switches[6],switches[5],switches[4],switches[3],switches[2],switches[1],switches[0]);
+			}
+
+		vTaskDelay(1000);
+	}
+}
+
 static void WallSwitchPoll(void *pvParameters) {
+
 	unsigned int CurrSwitchValue = 0;
 	unsigned int PrevSwitchValue = 0;
 	unsigned int temp = 100;
 
   while (1){
-	  
     // read the value of the switch
     CurrSwitchValue = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE) & 0x7F;
 
     if (CurrSwitchValue != PrevSwitchValue ) {
-        
+
         if (xQueueSend(newLoadQ, &CurrSwitchValue, 10) == pdTRUE) {
-			xQueueReceive(newLoadQ, &temp, portMAX_DELAY);
-        	printf("%d \n", temp);
+        	PrevSwitchValue = CurrSwitchValue;
         } else {
             printf("failed send \n");
         }
     }
-  vTaskDelay(100);
 
+  vTaskDelay(100);
   }
 
 }
 
 int CreateTasks() {
 	xTaskCreate(WallSwitchPoll, "SwitchPoll", TASK_STACKSIZE, NULL, 1, NULL);
+	xTaskCreate(load_Manage,"Load_Management",TASK_STACKSIZE, NULL, 2,NULL);
 	return 0;
 }
  
@@ -97,7 +124,6 @@ void freq_relay(){
 int main(int argc, char* argv[], char* envp[])
 {
 	printf("hello from Nios II");
-
 
 	// Register interrupt for frequency analyser component
 	alt_irq_register(FREQUENCY_ANALYSER_IRQ, 0, freq_relay);
