94830b98bb17d847410733531a095a390b767d9e
Nikhil <nikhil@pop-os.localdomain>
2021-04-21 18:39:17 +1200

    Cleaning

--- Software/Assignment01/main.c
+++ Software/Assignment01/main.c
@@ -1,4 +1,17 @@
 
+// Standard includes
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+// Scheduler includes
+#include "FreeRTOS/FreeRTOS.h"
+#include "FreeRTOS/task.h"
+#include "FreeRTOS/queue.h"
+#include "FreeRTOS/semphr.h"
+
+#include <altera_avalon_pio_regs.h>
+
 // Definition of Task Stacks
 #define   TASK_STACKSIZE       2048
 
@@ -26,6 +39,8 @@ QueueHandle_t newLoadQ ;
 int main(int argc, char* argv[], char* envp[]){
     return 0;
 }
+
+
 
 
 
